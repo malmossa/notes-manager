a199629a8de9f9593ad4ff7b7afec44bd3a799af
Mansor Almossa <malmossa@gmail.com>
2022-05-19 20:00:53 -0700

    Completed all routes

--- server/index.js
+++ server/index.js
@@ -14,10 +14,63 @@ const db = require("./db");
 const port = process.env.PORT || 5001;
 
 // Get All Notes
-app.get("/api/notes", async (req, res) => {
+app.get("/notes", async (req, res) => {
   try {
     const results = await db.query("SELECT * FROM note");
     res.json(results.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// Get One Note
+app.get("/notes/:id", async (req, res) => {
+  try {
+    const results = await db.query("SELECT * FROM note WHERE note_id = $1", [
+      req.params.id,
+    ]);
+
+    res.json(results.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// Create A Note
+app.post("/create", async (req, res) => {
+  try {
+    const results = await db.query(
+      "INSERT INTO note (note_description) VALUES ($1) RETURNING *",
+      [req.body.note_description]
+    );
+    res.json(results.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// Update A Note
+app.put("/notes/:id", async (req, res) => {
+  try {
+    const results = await db.query(
+      "UPDATE note SET note_description =$1 WHERE note_id = $2 RETURNING *",
+      [req.body.note_description, req.params.id]
+    );
+
+    res.json(results.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//Delete A Note
+app.delete("/notes/:id", async (req, res) => {
+  try {
+    const results = await db.query("DELETE FROM note WHERE note_id = $1", [
+      req.params.id,
+    ]);
+
+    res.json("Note was deleted");
   } catch (err) {
     console.error(err.message);
   }
