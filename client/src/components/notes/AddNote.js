98911eeeebcd7d181ad33cdb1bab11c0edda9c85
Mansor Almossa <malmossa@gmail.com>
2022-05-21 05:47:44 -0700

    add note input completed

--- client/src/components/notes/AddNote.js
+++ client/src/components/notes/AddNote.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from "react";
 
-import { TextField, Box, Button } from "@mui/material";
+import { Box, TextField, Button } from "@mui/material";
 
 function AddNote() {
   return (
     <Fragment>
       <Box sx={{ width: 500, maxWidth: "100%" }} m="auto">
-        <TextField multiline rows={4} fullWidth label="Add new note" />
+        <TextField label="Add new note" fullWidth multiline />
         <Button fullWidth sx={{ mt: 1 }} variant="contained">
           Add
         </Button>
