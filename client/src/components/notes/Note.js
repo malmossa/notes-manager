e486f9395a5bc4edbaf0829257cba5726cf1e4af
Mansor Almossa <malmossa@gmail.com>
2022-05-22 08:58:02 -0700

    Completed

--- client/src/components/notes/Note.js
+++ client/src/components/notes/Note.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Paper, Typography } from "@mui/material";
 
 import "./style.css";
 
 function Note() {
   return (
-    <Paper className="padding" sx={{ position: "relative" }}>
-      <Typography variant="body2">
-        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-        blanditiis tenetur unde suscipit, quam beatae rerum inventore
-        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-        fugiat deleniti? Eum quasi quidem quibusdam.
-      </Typography>
-    </Paper>
+    <Fragment>
+      <Paper className="padding" sx={{ position: "relative" }}>
+        <Typography variant="body2">
+          body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+          blanditiis tenetur unde suscipit, quam beatae rerum inventore
+          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+          fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+      </Paper>
+    </Fragment>
   );
 }
 
